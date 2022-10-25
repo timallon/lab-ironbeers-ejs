2cb1e0073f5b2b8966924ddcd4789b793faca2e7
Tim Allon <timallon@gmail.com>
2022-10-25 22:40:23 +0100

    iteration-4 I give up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
@@ -24,11 +25,20 @@ app.get('/beers', (req, res) => {
   .then(beersFromApi => {
     console.log('Beers from the database: ', beersFromApi)
     res.render('beers', {beers: beersFromApi});
-
   })
-
   .catch(error => console.log(error));
-  
   });
 
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log('Random beer from the database: ', responseFromAPI)
+    res.render('random-beer', {beer: responseFromAPI});
+    // your magic happens here
+  })
+  .catch(error => console.log(error));
+  });  
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
